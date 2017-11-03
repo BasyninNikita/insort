c65e494dbe3b95418b39dc967e298886515fd1b6
BasyninNikita <32190908+BasyninNikita@users.noreply.github.com>
2017-11-03 09:46:42 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <utility>
+#include <sstream>
+using namespace std;
+void vyvod(int *array,unsigned int size)
+{
+	for(unsigned int i=0;i<size;i++)
+	{
+		cout<<array[i]<<" ";
+	}
+}
+bool vvod(int *array,unsigned int size)
+{
+    string stroka;
+    getline(cin,stroka);
+    istringstream stream(stroka);
+    for(unsigned int i=0;i<size;i++)
+    {
+        if(!(stream>>array[i])){
+            cout<<"nepolnuy massiv";
+            return false;
+        }   
+    }
+    return true;
+}
+void insort(int *array,unsigned int size)
+{
+	for(unsigned int i=0;i<size;i++)
+	{
+		for(unsigned int j=i;j>0;j--)
+		{
+			if(array[j]<array[j-1]){swap(array[j-1],array[j]);} else break;
+		}
+	}
+}
+unsigned int size;
+int main(int argc, char** argv) {
+	cin>>size;
+	cin.get();	
+	int *array=new int[size];
+	if(vvod(array,size))
+	{
+		insort(array,size);
+		vyvod(array,size);
+	}
+	delete[]array;
+	return 0;
+}
